cfd3be807752eba70a9eb0e16d4587b7baa666ab
Rafael Brasil <rafaelpontesbr@gmail.com>
2019-11-18 15:31:43 -0400

    future build

--- lib/Message.dart
+++ lib/Message.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 
 part 'Message.g.dart';
 
@@ -11,4 +14,18 @@ class Message {
 
   factory Message.fromJson(Map<String, dynamic> json)
     => _$MessageFromJson(json);
+
+  static Future<List<Message>> browse() async {
+    // Get data from a file
+    // String content = await rootBundle.loadString('data/message.json');
+    http.Response response = await http.get('http://www.mocky.io/v2/5dd2def633000022ae7a3ef0');
+
+    await Future.delayed(Duration(seconds: 3));
+
+    String content = response.body;
+    List collection = json.decode(content);
+    List<Message> _messages = collection.map((json) => Message.fromJson(json)).toList();
+
+    return _messages;
+  }
 }
--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -17,25 +17,13 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
-  var messages = const [];
-
-  Future loadMessageList() async {
-    // Get data from a file
-    // String content = await rootBundle.loadString('data/message.json');
-    http.Response response = await http.get('http://www.mocky.io/v2/5dd2def633000022ae7a3ef0');
-    String content = response.body;
-    List collection = json.decode(content);
-    List<Message> _messages = collection.map((json) => Message.fromJson(json)).toList();
-
-    setState(() {
-      messages = _messages;
-    });
-  }
+  Future<List<Message>> messages;
 
   @override
   void initState() {
-    loadMessageList();
     super.initState();
+
+    messages = Message.browse();
   }
 
   @override
@@ -43,27 +31,49 @@ class _MessageListState extends State<MessageList> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => Divider(),
-        itemCount: messages.length,
-        itemBuilder: (BuildContext context, int index) {
-          Message message = messages[index];
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {
+            var _messages = Message.browse();
 
-          return ListTile(
-            title: Text(message.subject),
-            isThreeLine: true,
-            leading: CircleAvatar(
-              child: Text('AV'),
-            ),
-            subtitle: Text(
-              message.body,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          );
+            setState(() {
+              messages = _messages;
+            });
+          })
+        ],
+      ),
+      body: FutureBuilder(
+        future: messages,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+            if (snapshot.hasError) return Text('There was an error: ${snapshot.error}');
+            var messages = snapshot.data;
+              return ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Message message = messages[index];
+
+                  return ListTile(
+                    title: Text(message.subject),
+                    isThreeLine: true,
+                    leading: CircleAvatar(
+                      child: Text('AV'),
+                    ),
+                    subtitle: Text(
+                      message.body,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  );
+                },
+              );
+          }
         },
-      )
-    );
+      ));
   }
 }
