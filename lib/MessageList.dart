1948b316bb116acbc86f8487aba462292f23624e
Rafael Brasil <rafaelbrasil@csc.gruporovema.com.br>
2020-07-16 16:25:24 -0400

    e14: add drawer

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -4,6 +4,7 @@ import 'package:emailapp/Message.dart';
 import 'package:emailapp/MessageCompose.dart';
 import 'package:emailapp/MessageDetail.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 // Get data from a file
 // import 'package:flutter/services.dart';
 
@@ -46,8 +47,68 @@ class _MessageListState extends State<MessageList> {
           })
         ],
       ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountEmail: Text('myemail@email.com'),
+                accountName: Text('Daimioo'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://avatars3.githubusercontent.com/u/28401020?s=460&u=a8984d5abd9cd0f1cd8203e0d07fb928ccc05c36&v=4"
+                  ),
+                ),
+              otherAccountsPictures: <Widget>[
+                GestureDetector(
+                  onTap: () => showDialog(context: context, builder: (context) {
+                    return AlertDialog(title: Text("Adding new account..."));
+                }),
+                  child: CircleAvatar(
+                    child: Icon(Icons.add),
+                  )
+                ),
+              ],
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.inbox),
+              title: Text("Inbox"),
+              trailing: Chip(
+                  label: Text("11", style: TextStyle(fontWeight: FontWeight.bold)),
+                  backgroundColor: Colors.blue[100]
+              ),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.edit),
+              title: Text("Draft"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.archive),
+              title: Text("Archive"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.paperPlane),
+              title: Text("Sent"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.trash),
+              title: Text("Trash"),
+            ),
+            Divider(),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                  child: ListTile(
+                    leading: Icon(FontAwesomeIcons.cog),
+                    title: Text("Settings"),
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
       body: FutureBuilder(
         future: future,
+        // ignore: missing_return
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
@@ -75,8 +136,10 @@ class _MessageListState extends State<MessageList> {
                       overflow: TextOverflow.ellipsis,
                     ),
                     onTap: () {
-                      Navigator.push(context,
-                       MaterialPageRoute(builder: (BuildContext context) => MessageDetail(message.subject, message.body)));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (BuildContext context) => MessageDetail(message.subject, message.body))
+                      );
                     },
                   );
                 },
